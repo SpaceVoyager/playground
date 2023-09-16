857c95b4a46733002cc4952f1ab0a9774e40b3d3
Yuhang Wang <yuhangw@gmail.com>
2023-09-16 05:17:28 -0400

    Updated redis_test.py

--- redis_test.py
+++ redis_test.py
@@ -1,12 +1,14 @@
 import redis
 
-pool = redis.ConnectionPool(host='newpi4.local', port=6379, db=0)
-redis = redis.Redis(connection_pool=pool)
+r = redis.StrictRedis(host='newpi4.local', port=6379)
 
-redis.set('mykey', 'Hello from Python!')
-value = redis.get('mykey')
+r.set('mykey', 'Hello from Python!')
+value = r.get('mykey')
 print(value)
-redis.zadd('leaderboard', {'Luke' : 5563})
-redis.zadd('leaderboard', {'Yuhang' : 800})
-leaderboard = redis.zrange('leaderboard', 0, -1, withscores=True)
+
+r.zadd('leaderboard', {'Luke' : 5563})
+r.zadd('leaderboard', {'Yuhang' : 800})
+leaderboard = r.zrange('leaderboard', 0, -1, withscores=True)
 print(leaderboard)
+
+r.connection_pool.disconnect()
